e19831ad434af5b1c8addf8476e63d320df1831b
Julio-Martel <julioramdom123@gmail.com>
2026-03-10 18:34:28 -0300

    se ha creado el archivo videojuegos.js

---
+++ videojuegos/videojuegos.js
@@ -0,0 +1,74 @@
+const videojuegos = [
+  {
+    codigo: 1,
+    nombre: "Resident Evil 4",
+    plataforma: "PS5",
+    precio: 60,
+    stock: 10
+  },
+  {
+    codigo: 2,
+    nombre: "The Last of Us Part I",
+    plataforma: "PS5",
+    precio: 70,
+    stock: 8
+  },
+  {
+    codigo: 3,
+    nombre: "God of War Ragnarok",
+    plataforma: "PS5",
+    precio: 70,
+    stock: 12
+  },
+  {
+    codigo: 4,
+    nombre: "Elden Ring",
+    plataforma: "PC",
+    precio: 60,
+    stock: 15
+  },
+  {
+    codigo: 5,
+    nombre: "Cyberpunk 2077",
+    plataforma: "PC",
+    precio: 50,
+    stock: 9
+  },
+  {
+    codigo: 6,
+    nombre: "Red Dead Redemption 2",
+    plataforma: "Xbox",
+    precio: 40,
+    stock: 7
+  },
+  {
+    codigo: 7,
+    nombre: "Halo Infinite",
+    plataforma: "Xbox",
+    precio: 60,
+    stock: 11
+  },
+  {
+    codigo: 8,
+    nombre: "Minecraft",
+    plataforma: "Multiplataforma",
+    precio: 30,
+    stock: 20
+  },
+  {
+    codigo: 9,
+    nombre: "Fortnite",
+    plataforma: "Multiplataforma",
+    precio: 0,
+    stock: 999
+  },
+  {
+    codigo: 10,
+    nombre: "Call of Duty Modern Warfare III",
+    plataforma: "PS5",
+    precio: 70,
+    stock: 6
+  }
+];
+
+module.exports = videojuegos;
