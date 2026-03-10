94040375ef16dbc3f53f4378e570652fe4019bf6
Julio-Martel <julioramdom123@gmail.com>
2026-03-10 18:38:08 -0300

    se ha creado el post

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const videojuegos = require("./videojuegos/videojuegos");
 
 app.use(express.json());
 
+app.get("/videojuegos",(req,res) => {
+    res.json(videojuegos);
+})
 
-
-
+app.post("/videojuegos/:codigo",verificarCodigoVideojuego,(req,res) => {
+    
+})
 
 app.listen(3000,() => {
     console.log("Servidor activo");
