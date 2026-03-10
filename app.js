761827d9566664577d4b36c4d30cab0ac6530a19
Julio-Martel <julioramdom123@gmail.com>
2026-03-10 19:05:04 -0300

    se han implementado un middleware con exito mas el metodo htpp

--- app.js
+++ app.js
@@ -8,8 +8,48 @@ app.get("/videojuegos",(req,res) => {
     res.json(videojuegos);
 })
 
-app.post("/videojuegos/:codigo",verificarCodigoVideojuego,(req,res) => {
-    
+const verificarCodigoVideojuego = (req,res,next) => {
+    const codigo = parseInt(req.params.codigo);
+    const buscarCodigo = videojuegos.find(v => v.codigo === codigo);
+
+    if(!buscarCodigo){
+        res.status(204).json({
+            mensaje: "Codigo de videojuego inexistente"
+        })
+    }
+
+    next();
+}
+
+const verificarNombreDelJuego = (req,res,next) => {
+    const nombre = req.body.nombre;
+    const verificarNombreDuplicado = videojuegos.find(v => v.nombre === nombre);
+
+    if(verificarNombreDuplicado){
+       return res.status(409).json({
+            mensaje:"El nombre del libro ya existe"
+        })
+    }
+
+    next()
+}
+
+app.post("/videojuegos",verificarNombreDelJuego,(req,res) => {
+   
+    const nuevoJuego = {
+        codigo: videojuegos.length + 1,
+        nombre: req.body.nombre,
+        plataforma: req.body.plataforma,
+        precio: req.body.precio,
+        stock: req.body.stock
+    }   
+
+    videojuegos.push(nuevoJuego);
+
+    res.status(200).json({
+        mensaje: "Videojuego añadido a la base de datos"
+    });
+
 })
 
 app.listen(3000,() => {
